8b4653180b34a4b8f60c306889e04cd644c2321e
Angelm25 <MejiaA25@gmail.com>
2013-09-30 23:39:59 -0400

    addition of portal class

--- ClassesFile.py
+++ ClassesFile.py
@@ -58,13 +58,12 @@ class Ball():
 #-----HAND CLASS----------------------------------------------------------
 class Hand():
 	def __init__(self, image, size=(50, 50), pos=(0, 0), bounds=(350,350)):
-<<<<<<< HEAD
+
 		
 		self.image = image
-=======
+
 		#initializer for the Hand class
 		
->>>>>>> f0be893f7c921079fa87876e2c48f5c1733b3827
 		self.hx, self.hy = pos
 		self.width, self.height = size
 		self.xBound, self.yBound = bounds
@@ -80,7 +79,7 @@ class Hand():
 
 		return(self.x, self.y)
 
-<<<<<<< HEAD
+
 	def openToClose(self):
 		
 		if not self.ball.surrounds((scaledX, scaledY)):
@@ -90,14 +89,62 @@ class Hand():
 
 	#def update
 
-=======
+
 	#def update(self, vhx, vhy):
 	
 		
->>>>>>> f0be893f7c921079fa87876e2c48f5c1733b3827
 	def surrounds(self, pointer_pos):
 		x, y = pointer_pos
 		if x < (self.hx + self.width) and x > (self.hx) and y < (self.hy + self.height) and y > (self.hy):
 			return True
 
 #-----HAND CLASS END------------------------------------------------------
+
+
+#-----PORTAL CLASS--------------------------------------------------------
+
+
+class oportal():
+	def __init__(self, image, size=(50, 50), pos=(300, 0), bounds=(350, 350)):
+		self.image = image
+
+		self.x, self.y = pos
+		self.width, self.height = size
+		self.xBound, self.yBound = bounds
+
+		self.vX = 0
+		self.vY = 0
+
+		self.state = 0 # 0 = Falling, 1 = Rising
+
+	def move(self):
+		self.x += self.vX
+		self.y += self.vY
+
+		self.update()
+
+		return (self.x, self.y)
+
+	def update(self):
+		if self.y > self.yBound:
+			self.y = self.yBound
+			if self.state == 0:
+				self.state = 1
+			else:
+				self.state = 0
+
+			self.vY = -self.vY
+
+		if self.x > self.xBound or self.x < 0:
+			self.x = self.xBound
+
+		self.vY += .05 # Gravity
+		self.vX *= .9 # Friction
+
+	def surrounds(self, pointer_pos):
+		x, y = pointer_pos
+		if x < (self.x + self.width) and x > (self.x) and y < (self.y + self.height) and y > (self.y):
+			return True
+		
+#------ END portal Class----------------------------------------------------------------------------------
+	
