f0be893f7c921079fa87876e2c48f5c1733b3827
employedtitan <wiberg_sven@wheatoncollege.edu>
2013-09-30 23:04:15 -0400

    Added some comments

--- ClassesFile.py
+++ ClassesFile.py
@@ -4,26 +4,31 @@
 #-----BALL CLASS----------------------------------------------------------
 class Ball():
 	def __init__(self, image, size=(50, 50), pos=(0, 0), bounds=(350, 350)):
-		self.image = image
+		#Initializer method for Ball class
+		
+		self.image = image #sets the image of the ball
 
-		self.x, self.y = pos
-		self.width, self.height = size
-		self.xBound, self.yBound = bounds
+		self.x, self.y = pos #puts the ball in the starting position
+		self.width, self.height = size #gives the ball its size
+		self.xBound, self.yBound = bounds #sets the boundaries for where the ball can go in the screen
 
-		self.vX = 0
-		self.vY = 0
+		self.vX = 0 #sets the initial velocity of the ball in the horizontal to 0
+		self.vY = 0 #sets the inital velocity of the ball in the vertical to 0
 
 		self.state = 0 # 0 = Falling, 1 = Rising
 
 	def move(self):
-		self.x += self.vX
-		self.y += self.vY
+		#updates the position of the ball
+		self.x += self.vX #updates the x coordinate
+		self.y += self.vY #updates the y coordinate
 
-		self.update()
+		self.update()#calls the Ball class update method
 
-		return (self.x, self.y)
+		return (self.x, self.y) #returns the new location
 
 	def update(self):
+		#The update method for the Ball class
+		
 		if self.y > self.yBound:
 			self.y = self.yBound
 			if self.state == 0:
@@ -40,8 +45,12 @@ class Ball():
 		self.vX *= .9 # Friction
 
 	def surrounds(self, pointer_pos):
-		x, y = pointer_pos
-		if x < (self.x + self.width) and x > (self.x) and y < (self.y + self.height) and y > (self.y):
+		#surrounds method checks to see if your finger is holding the ball
+		
+		x, y = pointer_pos  #position of the pointer
+		
+		if x < (self.x + self.width) and x > (self.x) and y < (self.y + self.height) and y > (self.y): #if statement to check to see if your finger and the ball share the same coordinates
+		#returns a boolean
 			return True
 #-----BALL CLASS END------------------------------------------------------
 
@@ -49,12 +58,14 @@ class Ball():
 #-----HAND CLASS----------------------------------------------------------
 class Hand():
 	def __init__(self, image, size=(50, 50), pos=(0, 0), bounds=(350,350)):
+		#initializer for the Hand class
+		
 		self.hx, self.hy = pos
 		self.width, self.height = size
 		self.xBound, self.yBound = bounds
 
 		self.vhx = 0
-		self.vhx = 0
+		self.vhy = 0
 
 	def move(self):
 		hx+=vhx
@@ -64,8 +75,9 @@ class Hand():
 
 		return(self.x, self.y)
 
-	#def update
-
+	#def update(self, vhx, vhy):
+	
+		
 	def surrounds(self, pointer_pos):
 		x, y = pointer_pos
 		if x < (self.hx + self.width) and x > (self.hx) and y < (self.hy + self.height) and y > (self.hy):
